c0e270a62988dde73ca35db4b87d7c4f9ebac373
jatinssh <jatin7838066@gmail.com>
2024-01-08 21:52:41 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,8 @@ import './App.css'
 import Home from './Home'
 import SignUp from './SignUp'
 import Login from './Login'
-import Error from'./Error'
 import TodoList  from './todo'
-import {createBrowserRouter,Router,RouterProvider} from 'react-router-dom'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +12,6 @@ function App() {
     {path:"/",element:<Home/>},
     {path:"/signup",element:  <SignUp/>},
     {path:"/login",element:<Login/>},
-    {errorElement:"/error",element: <Error/>},
     {path:"/todo",element:<TodoList/>}
   ])
 
